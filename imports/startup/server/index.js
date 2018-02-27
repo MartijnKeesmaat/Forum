1bc4ecffec6418e592c0fe603f4684cac052f57c
Martijn Keesmaat <martijn@bit-students.com>
2018-02-27 23:25:57 +0100

    Most basic schema

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,25 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({});
+// Schema, define the outcome of a key
+const typeDefs = `
+type Query {
+  hi: String
+}
+`;
+
+// Query
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello World";
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+createApolloServer({ schema });
