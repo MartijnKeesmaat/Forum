49f8d0ae6510dc3efc8015edea3dd3a89bfc98f7
Martijn Keesmaat <martijn@bit-students.com>
2018-02-27 23:47:41 +0100

    Link client to server (fe/be)

--- imports/startup/client/index.jsx
+++ imports/startup/client/index.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import App from '../../ui/App';
 
+// Link client to server (fe/be)
+const httpLink = new HttpLink({
+  uri: Meteor.absoluteUrl('graphql')
+})
+
+// create cache
+const cache = new InMemoryCache();
+
+// combine
+const client = new ApolloClient({
+  link: httpLink,
+  cache
+});
+
+// Render app in Apollo
+const ApolloApp = () => (
+  <ApolloProvider client ={client}>
+    <App />
+  </ApolloProvider>
+)
+
+// Render Apollo app
 Meteor.startup(() => {
-  render(<App />, document.getElementById('app'));
+  render(<ApolloApp />, document.getElementById('app'));
 });
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <h1>Hello world</h1>
+  <h1>Hello!</h1>
 );
 
 export default App;
