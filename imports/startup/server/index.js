823d125d78222d0e687ff81512077ebb901e3888
Martijn Keesmaat <martijn@bit-students.com>
2018-02-28 22:36:52 +0100

    First query in react

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,5 +1,5 @@
-import { createApolloServer } from 'meteor/apollo';
-import { makeExecutableSchema } from 'graphql-tools';
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
 
 // Schema, define the outcome of a key
 const typeDefs = `
@@ -15,11 +15,13 @@ const resolvers = {
       return "Hello World";
     }
   }
-}
+};
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers
-})
+});
 
-createApolloServer({ schema });
+createApolloServer({
+  schema
+});
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 
-const App = () => (
-  <h1>Hello!</h1>
-);
+const App = ({ data }) => <h1>{data.hi}</h1>;
 
-export default App;
+// Query from Graphiql
+const hiQuery = gql`
+  {
+    hi
+  }
+`;
+
+export default graphql(hiQuery)(App);
